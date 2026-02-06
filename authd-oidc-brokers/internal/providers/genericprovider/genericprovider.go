cb54f0545504815407ba13640e6073d3806ccff9
Adrian Dombeck <adrian.dombeck@canonical.com>
2026-02-06 13:46:30 +0100

    provider: Only use verified email as username
    Keycloak allows users to change their email address themselves without
    verifying it first. The ID token includes an `email_verified` claim
    which we now check and return an error if it's false, to avoid that
    users can log in with arbitrary usernames.
    
    This method of the genericprovider is also used by the Google provider
    which also includes the `email_verified` claim in the ID token.
    
    The `email_verified` claim is in fact defined as a standard claim in the
    OIDC spec (which doesn't mean that all IdPs have to set it).

--- authd-oidc-brokers/internal/providers/genericprovider/genericprovider.go
+++ authd-oidc-brokers/internal/providers/genericprovider/genericprovider.go
@@ -48,6 +48,10 @@ func (p GenericProvider) GetUserInfo(idToken info.Claimer) (info.User, error) {
 		return info.User{}, err
 	}
 
+	if !userClaims.EmailVerified {
+		return info.User{}, &providerErrors.ForDisplayError{Message: "Authentication failure: email not verified"}
+	}
+
 	return info.NewUser(
 		userClaims.Email,
 		userClaims.Home,
@@ -83,11 +87,12 @@ func (p GenericProvider) SupportedOIDCAuthModes() []string {
 }
 
 type claims struct {
-	Email string `json:"email"`
-	Sub   string `json:"sub"`
-	Home  string `json:"home"`
-	Shell string `json:"shell"`
-	Gecos string `json:"gecos"`
+	Email         string `json:"email"`
+	Sub           string `json:"sub"`
+	Home          string `json:"home"`
+	Shell         string `json:"shell"`
+	Gecos         string `json:"gecos"`
+	EmailVerified bool   `json:"email_verified"`
 }
 
 // userClaims returns the user claims parsed from the ID token.
